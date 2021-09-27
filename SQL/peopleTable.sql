31aa9e1756a344822a3d3af79dc6b95e5e5b6795
Aaron Ramirez <amramirez1222@gmail.com>
2021-09-27 13:43:22 -1000

    People sql

---
+++ SQL/peopleTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Events.people (
+	id INT NOT NULL,
+	Name varchar(100) NOT NULL,
+	`Rank` varchar(100) NOT NULL,
+	Email varchar(100) NOT NULL,
+	PhoneNumber varchar(100) NOT NULL
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
