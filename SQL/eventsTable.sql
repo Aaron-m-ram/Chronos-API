7cd96c72c7e7e1ac48e93691ce3dab9ff19b5058
Aaron Ramirez <amramirez1222@gmail.com>
2021-09-27 11:27:25 -1000

    event table added

---
+++ SQL/eventsTable.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Events.events (
+	id INT NOT NULL,
+	Name varchar(100) NOT NULL,
+	`Date` varchar(100) NOT NULL,
+	`Start` varchar(100) NOT NULL,
+	`End` varchar(100) NOT NULL,
+	Seats INT NOT NULL,
+	Reoccurring varchar(100) NOT NULL,
+	CONSTRAINT events_PK PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
