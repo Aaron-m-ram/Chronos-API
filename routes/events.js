5ef4ccb3e6f20b313887bd588dcd10cd26bdb50b
Aaron Ramirez <amramirez1222@gmail.com>
2021-09-30 08:21:24 -1000

    Router post and get by ID
    Can now post to the full table as well as get each event by id

--- routes/events.js
+++ routes/events.js
@@ -2,17 +2,6 @@ import express from "express";
 import pool from "../helpers/database.js";
 const router = express.Router();
 
-/* router.get("/:id", async function (req, res) {
-  try {
-    const sqlQuery =
-      "SELECT id, Name, Date, Start, End, Seats, Reoccuring FROM events WHERE id=?";
-    const rows = await pool.query(sqlQuery, req.params.id);
-    return res.status(200).json(rows);
-  } catch (error) {
-    return res.status(400).send(error.message);
-  }
-}); */
-
 router.get("/table", async function (req, res) {
   try {
     const sqlQuery = " SELECT * FROM events";
@@ -26,10 +15,31 @@ router.get("/table", async function (req, res) {
 router.post("/table", async function (req, res) {
   try {
     const singleEvent = req.body;
-    const sqlQuery = `INSERT INTO events (id, Name, Date, Start, End, Seats, Reoccuring) VALUES (${singleEvent.id} ${singleEvent.Name} ${singleEvent.Date} ${singleEvent.Start} ${singleEvent.End} ${singleEvent.Seats} ${singleEvent.Reoccuring})`;
-    const result = await pool.query(sqlQuery);
+    const sqlQuery =
+      "INSERT INTO events (id, Name, Date, Start, End, Seats, Reoccuring) VALUES(?,?,?,?,?,?,?)";
+    const result = await pool.query(sqlQuery, [
+      singleEvent.id,
+      singleEvent.Name,
+      singleEvent.Date,
+      singleEvent.Start,
+      singleEvent.End,
+      singleEvent.Seats,
+      singleEvent.Reoccuring,
+    ]);
 
     return res.status(200).json(result);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
+
+// /event/21321313
+router.get("/:id", async (req, res) => {
+  try {
+    const sqlQuery =
+      "SELECT id, Name, Date, Start, End, Seats, Reoccuring FROM events WHERE id=?";
+    const rows = await pool.query(sqlQuery, req.params.id);
+    return res.status(200).json(rows);
   } catch (error) {
     return res.status(400).send(error.message);
   }
